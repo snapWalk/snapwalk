f9f363ec224ba29abdaf73a42b1205546dc86568
Mike Chabot <mike.chabot@live.com>
2016-04-28 20:10:25 -0400

    Addin json-loader, removing react-tap-event-plugin

--- src/bootstrap.jsx
+++ src/bootstrap.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import babel from 'babel-polyfill';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import styles from './scss/style.scss';
 import routes from './routes';
 import configureStore from './redux/store/configure-store';
 import { DEFAULT_STATE } from './common/app-const';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router'
-
-// Needed for onTouchTap
-// Can go away when react 1.0 release
-// Check this repo:
-// https://github.com/zilverline/react-tap-event-plugin
-injectTapEventPlugin();
-
 
 // Configure the Redux store
 const store = configureStore(DEFAULT_STATE);
--- src/common/app-const.js
+++ src/common/app-const.js
@@ -1,5 +1,4 @@
 export const DEFAULT_STATE = {
     model: {}
-}
+};
 
-export const BASE_URL = 'http://localhost:3040/api'
