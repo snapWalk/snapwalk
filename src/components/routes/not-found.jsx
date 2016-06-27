10056cbb65a616cd297f08a503e2294698335611
Mike Chabot <mike.chabot@live.com>
2016-06-27 06:38:09 -0400

    Logging props on not found

--- src/components/routes/not-found.jsx
+++ src/components/routes/not-found.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-export default function NotFound () {
+export default function NotFound (props) {
+    console.log(props);
     return (
         <section>
             <h5>404 Not Found</h5>
