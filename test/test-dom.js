d8e1bce2391876a68a5a3a47f47223fd4e2a8217
Mike Chabot <mike.chabot@live.com>
2017-05-04 16:15:23 -0400

    Updating dependencies and test utils

--- test/test-dom.js
+++ test/test-dom.js
@@ -1,7 +1,9 @@
 let jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
+const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+global.window = dom.window.document.defaultView;
+global.document = dom.window.document;
 global.navigator = {
     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
 };
