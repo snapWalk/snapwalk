0c134899c8f077a64946a8e371eda8a5e2a07026
Mike Chabot <mike.chabot@live.com>
2018-02-07 17:37:47 -0500

    Fixing component tests

--- test/unit/component/common/test-flexbox.js
+++ test/unit/component/common/test-flexbox.js
@@ -2,8 +2,7 @@ import React from 'react';
 import TestUtils from 'react-dom/test-utils';
 import { expect } from 'chai';
 import { renderStatelessComponent } from '../../../react-utils';
-
-import Flexbox from '../../../../src/components/common/Flexbox';
+import Flex from '../../../../src/components/common/glamorous/Flex';
 
 describe('Flexbox Component', () => {
     let props;
@@ -11,22 +10,11 @@ describe('Flexbox Component', () => {
         props = { id: 'ABC' };
     });
 
-    describe('<Flexbox />', () => {
-        it('Should return a flexbox row', () => {
-            const component = renderStatelessComponent(Flexbox, props);
+    describe('<Flex />', () => {
+        it('Should return a Flex component', () => {
+            const component = renderStatelessComponent(Flex, props);
             const element = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
             expect(element.id).to.equal(props.id);
-            expect(element.style.getPropertyValue('display')).to.equal('flex');
-        });
-    });
-    describe('<Flexbox column={true} />', () => {
-        it('Should return a flexbox column', () => {
-            props = { ...{ column: true }, ...props };
-            const component = renderStatelessComponent(Flexbox, props);
-            const element = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
-            expect(element.id).to.equal(props.id);
-            expect(element.style.display).to.equal('flex');
-            expect(element.style.flexDirection).to.equal('column');
         });
     });
 });
