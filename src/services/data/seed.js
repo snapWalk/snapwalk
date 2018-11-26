8a771490abc1379ba49eede48168ecda6b1d9cd4
Anna Kanska <ann.kanska@gmail.com>
2018-11-26 14:35:12 +0100

    created  CREATE TABLE query

--- src/services/data/seed.js
+++ src/services/data/seed.js
@@ -4,6 +4,6 @@ const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/
 const client = new pg.Client(connectionString);
 client.connect();
 
-const query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+const query = client.query('CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(20) UNIQUE, email VARCHAR(40) UNIQUE, password VARCHAR(200)); CREATE TABLE routes(id SERIAL PRIMARY KEY, name VARCHAR(20), description VARCHAR(400), author INTEGER); CREATE TABLE places(id SERIAL, name VARCHAR(20), place_order INTEGER, description VARCHAR(400), latitude VARCHAR(20), lonitude VARCHAR(20), item VARCHAR(20), routes INTEGER); ALTER TABLE routes ADD FOREIGN KEY(author) REFERENCES users(id); ALTER TABLE places ADD FOREIGN KEY(routes) REFERENCES routes(id)');
 
 query.on('end', () => { client.end(); });
