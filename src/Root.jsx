c010b57757500990e851338a8d44ee1819bf8bf0
NyanHorse <almudena.canalejo@gmail.com>
2018-11-26 14:40:23 +0100

    home view added

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './components/home';
 
 class App extends React.Component {
   render () {
     return (
-      <div>Hello Almudena and Anna!</div>
+      <div>
+        <Home/>
+      </div>
     );
   }
 }
