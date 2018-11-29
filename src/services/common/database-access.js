c1e803e85a7ffb04b80f209e5fb78bd018208bfc
Krista Moroder <kmoroder@users.noreply.github.com>
2018-11-29 15:04:40 +0100

    Revert styling changes in database-access file

--- src/services/common/database-access.js
+++ src/services/common/database-access.js
@@ -1,36 +1,32 @@
-const pg = require("pg");
-const connectionString =
-  process.env.DATABASE_URL || "postgres://localhost:5432/todos";
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todos';
 
-module.exports = async function getData(query) {
+module.exports = async function getData (query) {
   const data = [];
   let error = null;
 
   await new Promise((resolve, reject) => {
     // Get a Postgres client from the connection pool
-    pg.connect(
-      connectionString,
-      (err, client, done) => {
-        // Handle connection errors
-        if (err) {
-          done(); // Kills the pg connection
-          error = err; // Stores the error it came back with
-          resolve(); // Finishes the Promise
-        }
-        // Create SQL query
-        const q = client.query(query);
-        // Stream data back one row at a time
-        q.on("row", row => {
-          data.push(row);
-        });
-        // After all data is returned, close connection and resolve promise
-        q.on("end", () => {
-          done();
-          resolve();
-        });
+    pg.connect(connectionString, (err, client, done) => {
+      // Handle connection errors
+      if (err) {
+        done(); // Kills the pg connection
+        error = err; // Stores the error it came back with
+        resolve(); // Finishes the Promise
       }
-    );
+      // Create SQL query
+      const q = client.query(query);
+      // Stream data back one row at a time
+      q.on('row', (row) => {
+        data.push(row);
+      });
+      // After all data is returned, close connection and resolve promise
+      q.on('end', () => {
+        done();
+        resolve();
+      });
+    });
   });
 
   return { data, error };
-};
+}
