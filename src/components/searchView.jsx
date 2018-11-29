707c840608037f7f9ddc8cac286f1c45dc3fe465
Anna Kanska <ann.kanska@gmail.com>
2018-11-29 15:42:02 +0100

    added a route card in searchview

--- src/components/searchView.jsx
+++ src/components/searchView.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Thumbnail } from "react-bootstrap";
 
 class SearchView extends React.Component {
   constructor (props) {
@@ -10,6 +11,15 @@ class SearchView extends React.Component {
     return (
       <div>
         <h2>Search for a route</h2>
+        <Col>
+          <Thumbnail>
+            <h3>Route name</h3>
+            <p>Route description</p>
+            <p>
+              <Button bsStyle="primary">Start</Button>
+            </p>
+          </Thumbnail>
+        </Col>
       </div>
     );
   }
