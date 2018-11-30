8d1bc65682b2788b93f06257055f9800f63fef59
Anna Kanska <ann.kanska@gmail.com>
2018-11-30 13:11:13 +0100

    searchview bootstrap

--- src/components/searchView.jsx
+++ src/components/searchView.jsx
@@ -4,20 +4,25 @@ import { Button, Col, Thumbnail } from "react-bootstrap";
 class SearchView extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: null,
+      description: null
+    };
   }
 
   render () {
+    let name = this.state.name || "Sample Route Name";
+    let description = this.state.description || "Sample description here. Lalalaaaaaa. Lalaa.";
     return (
       <div>
         <h2>Search for a route</h2>
         <Col>
           <Thumbnail>
-            <h3>Route name</h3>
-            <p>Route description</p>
-            <p>
-              <Button bsStyle="primary">Start</Button>
-            </p>
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <div className="text-right">
+              <Button bsStyle="success">Start</Button>
+            </div>
           </Thumbnail>
         </Col>
       </div>
